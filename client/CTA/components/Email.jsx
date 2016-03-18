11c32b25d8dab5d6e0bdacd20fc29449ed52f078
xavizalote <xavier.cazalot@gmail.com>
2016-03-18 16:39:38 +0100

    give some content to the CTA steps

--- client/CTA/components/Email.jsx
+++ client/CTA/components/Email.jsx
@@ -4,7 +4,26 @@ export default class Email extends React.Component {
 
 	render () {
 		return (
-			<div>Enter your email!</div>
+			<form className="card z-depth-3" id="form-user" onSubmit={this.props.action}>
+				<div className="row">
+					<h4 className="center">Par ton email</h4>
+					<div className="input-field | col s12">
+						<input id="email" type="email" ref="email" />
+						<label htmlFor="email">Email</label>
+					</div>
+					<div className="col s12">
+						<button className="btn btn-large" type="submit" id="send_mail">Submit</button>
+					</div>
+				</div>
+				<div className="row">
+					<h4 className="center">OAuth</h4>
+					<div className="row">
+						<div className="col s12">
+							<button className="btn btn-large" type="submit" id="send_mail">OAUTH</button>
+						</div>
+					</div>
+				</div>
+			</form>
 		);
 	}
 }
--- client/CTA/components/Thanks.jsx
+++ client/CTA/components/Thanks.jsx
@@ -4,7 +4,14 @@ export default class Thanks extends React.Component {
 
 	render () {
 		return (
-			<div>Thanks!</div>
+			<form className="card z-depth-3" id="thanks">
+				<div className="row center">
+					<div className="icon-block">
+						<h2><img className="icon icons8-Action" src="http://cultofthepartyparrot.com/parrots/parrot.gif" width="70" height="50"/></h2>
+					</div>
+					<h2>YEAH</h2>
+				</div>
+			</form>
 		);
 	}
 }
