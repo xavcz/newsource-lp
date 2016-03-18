c7806f2c6d1e9491f6cec81023d17fca7054529b
xavizalote <xavier.cazalot@gmail.com>
2016-03-18 16:58:30 +0100

    accounts package

--- client/CTA/components/Email.jsx
+++ client/CTA/components/Email.jsx
@@ -4,26 +4,31 @@ export default class Email extends React.Component {
 
 	render () {
 		return (
-			<form className="card z-depth-3" id="form-user" onSubmit={this.props.action}>
+			<div className="card z-depth-3">
 				<div className="row">
-					<h4 className="center">Par ton email</h4>
-					<div className="input-field | col s12">
-						<input id="email" type="email" ref="email" />
-						<label htmlFor="email">Email</label>
-					</div>
-					<div className="col s12">
-						<button className="btn btn-large" type="submit" id="send_mail">Submit</button>
-					</div>
+					<form id="form-user" onSubmit={this.props.action}>
+						<h4 className="center">Par ton email</h4>
+						<div className="input-field | col s12">
+							<input id="email" type="email" ref="email" />
+							<label htmlFor="email">Email</label>
+						</div>
+						<div className="col s12">
+							<button className="btn btn-large" type="submit" id="send_mail">Submit</button>
+						</div>
+					</form>
 				</div>
 				<div className="row">
 					<h4 className="center">OAuth</h4>
 					<div className="row">
 						<div className="col s12">
-							<button className="btn btn-large" type="submit" id="send_mail">OAUTH</button>
+							<button class="btn btn-fab btn-primary">F</button>
+							<button class="btn btn-fab btn-info">T</button>
+							<button class="btn btn-fab btn-danger">G</button>
 						</div>
 					</div>
 				</div>
-			</form>
+			</div>
 		);
 	}
 }
+
