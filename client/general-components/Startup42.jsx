d884f335bb4e57131bbae28bc12733e4f4d63d92
Thomas Jeanneau <thomasj230@gmail.com>
2016-03-18 16:31:24 +0100

    change logo + add wording

--- client/general-components/Startup42.jsx
+++ client/general-components/Startup42.jsx
@@ -6,7 +6,7 @@ export default class Startup42 extends React.Component {
 			<div id="why" className="container">
 				<div id="because-I-do-everything" className="section section__bigflow center-align">
 					<h2 className="center">Avec la contribution et l'expertise de</h2>
-					<img src="/img/startUp42LogoFinal.png" height="70%" width="70%"/>
+					<img src="/img/logo_startup42-fond_clair.png" height="70%" width="70%"/>
 				</div>
 			</div>
 		);
