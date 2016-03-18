a7aa0abb77ce8a6cd16defae5459670d1a9efe44
Thomas Jeanneau <thomasj230@gmail.com>
2016-03-18 16:19:02 +0100

    add logo startup42

--- client/general-components/Startup42.jsx
+++ client/general-components/Startup42.jsx
@@ -4,11 +4,9 @@ export default class Startup42 extends React.Component {
 	render() {
 		return (
 			<div id="why" className="container">
-				<div id="because-I-do-everything" className="section section__bigflow">
-
-					<h2 className="center">Avec la contribution de Startup42, accélérateur de startups</h2>
-					<p className="flow-text">Ipsum nulla voluptas dicta nulla laudantium non. Aperiam hic facere officiis iusto iste. Maiores ipsam deserunt temporibus excepturi repudiandae vero minus temporibus. Minus ipsa nesciunt nulla corporis debitis veritatis ex? </p>
-
+				<div id="because-I-do-everything" className="section section__bigflow center-align">
+					<h2 className="center">Avec la contribution et l'expertise de</h2>
+					<img src="/img/startUp42LogoFinal.png" height="70%" width="70%"/>
 				</div>
 			</div>
 		);
