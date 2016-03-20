7b09ec2ae1d7755b315b150e650ca77fc2b9a1be
Thomas Jeanneau <thomasj230@gmail.com>
2016-03-20 21:20:40 +0100

    add responsivity

--- client/templates/common/Testimonials.jsx
+++ client/templates/common/Testimonials.jsx
@@ -10,19 +10,19 @@ export default class Testimonials extends React.Component {
 						<h3 className="center">Startupers ayant appliqué cette méthode</h3>
 						<div className="col s12 m12 l4">
 							<div className="icon-block center">
-								<h5 className="primary-text-color flow-text">On ne vient pas chercher des réponses… on vient savoir comment tacler notre problème.</h5>
+								<h5 className="primary-text-color flow-text">"On ne vient pas chercher des réponses… On vient savoir comment tacler notre problème."</h5>
 								<p className="light">— Térence, COO Ad Scientiam</p>
 							</div>
 						</div>
 						<div className="col s12 m12 l4">
 							<div className="icon-block center">
-								<h5 className="primary-text-color flow-text">Au lieu de faire les choses à l'envers, on exécute maintenant dans le bon sens!</h5>
+								<h5 className="primary-text-color flow-text">"Au lieu de faire les choses à l'envers, on exécute maintenant dans le bon sens !"</h5>
 								<p className="light">— Arthur, CTO Blackfoot Makers</p>
 							</div>
 						</div>
 						<div className="col s12 m12 l4">
 							<div className="icon-block center">
-								<h5 className="primary-text-color flow-text">J’ai eu l’impression d’apprendre un métier par semaine avec cette méthode.</h5>
+								<h5 className="primary-text-color flow-text">"J’ai eu l’impression d’apprendre un métier par semaine avec cette méthode."</h5>
 								<p className="light">— Thibault, CEO Lumière</p>
 							</div>
 						</div>
