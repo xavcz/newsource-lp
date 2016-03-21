7e50a2e7d041f2f7f072542c0ee7eb8974ea88a9
Thomas Jeanneau <thomasj230@gmail.com>
2016-03-21 16:19:54 +0100

    add wording

--- client/templates/CTA/components/Report.jsx
+++ client/templates/CTA/components/Report.jsx
@@ -7,19 +7,19 @@ export default class Report extends React.Component {
 			<form className="card z-depth-3" id="form-report" onSubmit={this.props.onSubmit}>
 				<div className="row">
 					<div className="input-field | col s12">
-						<textarea id="context" type="text" ref="context" placeholder="Marketplace sportive, réseau social géolocalisé, etc." className="materialize-textarea validate"/>
+						<textarea id="context" type="text" ref="context" placeholder="Décrit-nous ton projet: où est-ce que tu en es, depuis combien de temps as-tu commencé, tes objectifs, etc.." className="materialize-textarea validate"/>
 						<label htmlFor="context" className="active">Quel est le contexte de ton projet ?</label>
 					</div>
 				</div>
 				<div className="row">
 					<div className="input-field | col s12">
-						<textarea id="currentStatus" type="text" ref="currentStatus" placeholder="50 utilisateurs actifs par jour, 200€ de CA par semaine, etc." className="materialize-textarea validate"/>
+						<textarea id="currentStatus" type="text" ref="currentStatus" placeholder="Qu'est ce que tu mesures ? Qu'est ce qui te permet de dire si le projet avance, recul ou stagne ?" className="materialize-textarea validate"/>
 						<label htmlFor="currentStatus" className="active">Où en est la metric qui définit ton succès ?</label>
 					</div>
 				</div>
 				<div className="row">
 					<div className="input-field | col s12">
-						<textarea id="problem" type="text" ref="problem" placeholder="Comment savoir qui sont mes utilisateurs ? Comment générer plus de revenus ? Etc. ?" className="materialize-textarea validate"/>
+						<textarea id="problem" type="text" ref="problem" placeholder="Que veux-tu réaliser de concret pour faire avancer ton projet dans la semaine qui suis ?" className="materialize-textarea validate"/>
 						<label htmlFor="problem" className="active">Quel est le problème d'action que tu veux résoudre cette semaine ?</label>
 					</div>
 				</div>
--- client/templates/common/Startup42.jsx
+++ client/templates/common/Startup42.jsx
@@ -5,7 +5,8 @@ export default class Startup42 extends React.Component {
 		return (
 			<div id="why" className="container">
 				<div id="because-I-do-everything" className="section section__bigflow center-align">
-					<h1 className="center text-secondary-color">Avec la contribution et l'expertise de</h1>
+					<h1 className="center text-secondary-color">Méthode validée</h1>
+					<h3 className="center">Avec la contribution et l'expertise de</h3>
 					<img src="/img/logo_startup42-fond_clair.png" height="70%" width="70%"/>
 					<blockquote className="flow-text">
 						<p>En trois ans d'accélération de startups early-stage, Startup42 a développé une méthodologie qui permet aux entrepreneurs d'acquérir les bons reflexes en quelques semaines.</p>
